e91c0ae0e4ede658098923dd85ac2470c0f63f2f
Piotr Lisiak <31732848+piotr94lisiak@users.noreply.github.com>
2018-11-20 19:39:31 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def minus(self, x, y):
+        return x - y
+
+    def multiple(self, x, y):
+        return x * y
+
+    def devide(self, x, y):
+        # if y == 0:
+        #     return False
+        # else:
+        #     return x / y
+        return x / y
---
+++ CalculatorPyTest.py
@@ -0,0 +1,20 @@
+import pytest
+from Calculator import Calculator
+
+calculator = Calculator()
+
+def test_add():
+    assert calculator.add(2,2) == 4
+    assert calculator.add(3,1.3) == 4.3
+
+def test_minus():
+    assert calculator.minus(2,4) == -2
+    assert calculator.minus(0,4.2) == -4.2
+
+def test_multiple():
+    assert calculator.multiple(5,2) == 10
+    assert calculator.multiple(3.3,2) == 6.6
+
+def test_devide():
+    assert calculator.devide(9,3) == 3 
+    assert calculator.devide(6,0) == False
